d06b93274f240782c05a6f2f20b097611d917e4b
HoSoo Lee <hosoo.lee@arithnea.de>
2020-09-28 23:17:01 +0200

    31.Adding Layout Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+ad// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Das Leben in Deutschland Quiz App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(child: Text('Answer 1'), onPressed: null),
+            RaisedButton(child: Text('Answer 2'), onPressed: null),
+            RaisedButton(child: Text('Answer 3'), onPressed: null),
+          ],
+        ),
+      ),
+    );
   }
 }
