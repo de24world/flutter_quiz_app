f28d1fd451bb57b4786e23f6a56b9ab92fe4c883
HoSoo Lee <hosoo.lee@arithnea.de>
2020-09-30 02:21:47 +0200

    39. Creating a New, Custom Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 // void main() {
 //   runApp(MyApp());
 // }
@@ -10,11 +12,11 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
   void answerQuestion() {
@@ -37,7 +39,7 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(
+            Question(
               questions[questionIndex],
             ),
             RaisedButton(
